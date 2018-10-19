2c3c2678529ffd5a8edc472fd9476f98f9efc1c6
brooklyndadcore <scott.waring@gmail.com>
2018-10-19 14:20:15 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
   def show
     @post = Post.find(params[:id])
   end
@@ -9,10 +13,12 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
@@ -20,4 +26,5 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:title, :category, :content)
   end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  CAT_OPTIONS = ["Fiction", "Non-Fiction"]
+  validates :title, presence: true
+  validates :content, length: {minimum: 100, message: "Content is too short"}
+  validates :category, :inclusion => CAT_OPTIONS
 end
